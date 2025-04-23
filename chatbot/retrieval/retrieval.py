5c3ffdee30ed05b3b70d0fc73e8a52be87986e52
SeungMin Woo <woof0302@gmail.com>
2025-04-23 16:15:41 +0900

    retrieval.py

--- chatbot/retrieval/retrieval.py
+++ chatbot/retrieval/retrieval.py
@@ -2,7 +2,7 @@ import json
 
 
 class Retrieval:
-    def __init__(self, data_path: str = "./data/newjeans.json"):
+    def __init__(self, data_path: str = "../data/newjeans.json"):
         with open(data_path, "r") as file:
             data: dict[str, str] = json.load(file)
             self.data = data
@@ -23,6 +23,9 @@ class Retrieval:
         # query: this is an empty query (None of the json key matches)
         # @ return None
         # ###
+        for key in self.data:
+            if key.lower()in query.lower():
+                return f"{key}: {self.data[key]}"
         return None
 
 
